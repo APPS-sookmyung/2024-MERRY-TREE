72e845b5dc90013f085069ffb36a94029e4448f2
Bangjh05 <everangel05@naver.com>
2024-12-24 16:14:21 +0900

    Feat: design

--- ornament.js
+++ ornament.js
@@ -17,17 +17,17 @@
 //         }
 //     })
 
-const acquiredOrnaments = JSON.parse(localStorage.getItem('acquiredOrnaments'));
-const ornaments = document.querySelector('.ornaments');
+const acquiredOrnaments = JSON.parse(localStorage.getItem("acquiredOrnaments"));
+const ornaments = document.querySelector(".ornaments");
 
 console.log(acquiredOrnaments);
 
 acquiredOrnaments.forEach((imgSrc, index) => {
-    const ornamentDiv = document.createElement('div');
-    ornamentDiv.classList.add(`ornament_${index}`);
-    const ornamentImg = document.createElement('img');
-    ornamentImg.src = imgSrc;
-    ornamentImg.alt = `ornament_${index}`;
-    ornamentDiv.appendChild(ornamentImg);
-    ornaments.appendChild(ornamentDiv);
-})
+  const ornamentDiv = document.createElement("div");
+  ornamentDiv.classList.add(`ornament_${index}`);
+  const ornamentImg = document.createElement("img");
+  ornamentImg.src = imgSrc;
+  ornamentImg.alt = `ornament_${index}`;
+  ornamentDiv.appendChild(ornamentImg);
+  ornaments.appendChild(ornamentDiv);
+});
